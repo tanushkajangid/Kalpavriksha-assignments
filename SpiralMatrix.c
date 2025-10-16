308b192ecf31b3f71b65c0772a31c887d43c1265
Tanushka Jangid <tanushkajangid583@gmail.com>
2025-10-16 20:37:51 +0530

    Enhance spiral matrix program with input validation
    Added input validation for matrix size and improved error handling.

--- SpiralMatrix.c
+++ SpiralMatrix.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-void fillSpiralMatrix( int numberOfCells, int (*matrixPtr)[numberOfCells]){
+void fillSpiralMatrix( int numberOfCells, int (*matrixPointer)[numberOfCells]){
     int cellValue=1;
     int topRow=0, bottomRow= numberOfCells-1;
     int leftColumn=0, rightColumn= numberOfCells-1;
@@ -8,19 +8,19 @@ void fillSpiralMatrix( int numberOfCells, int (*matrixPtr)[numberOfCells]){
     while(topRow<= bottomRow && leftColumn<= rightColumn){
 
         for( int column=leftColumn; column<=rightColumn; column++){
-            *(*(matrixPtr+ topRow)+ column)= cellValue++;
+            *(*(matrixPointer+ topRow)+ column)= cellValue++;
         }
         topRow++;
 
         for( int row=topRow; row<=bottomRow; row++){
-            *(*(matrixPtr+ row)+ rightColumn)= cellValue++;
+            *(*(matrixPointer+ row)+ rightColumn)= cellValue++;
         }
         rightColumn--;
 
         if(topRow<=bottomRow){
             
             for( int column=rightColumn; column>=leftColumn; column--){
-                *(*(matrixPtr+ bottomRow)+ column)= cellValue++;
+                *(*(matrixPointer+ bottomRow)+ column)= cellValue++;
             }
             bottomRow--;
         }
@@ -28,14 +28,14 @@ void fillSpiralMatrix( int numberOfCells, int (*matrixPtr)[numberOfCells]){
         if(leftColumn<=rightColumn){
             
             for( int row=bottomRow; row>=topRow; row--){
-                *(*(matrixPtr+ row)+ leftColumn)= cellValue++;
+                *(*(matrixPointer+ row)+ leftColumn)= cellValue++;
             }
             leftColumn++;
         }
     }
 }
 
-void displayMatrixAs2DArray(int numberOfCells, int (*matrixPtr)[numberOfCells]){
+void displayMatrixAs2DArray(int numberOfCells, int (*matrixPointer)[numberOfCells]){
 
     printf("[");
 
@@ -45,7 +45,7 @@ void displayMatrixAs2DArray(int numberOfCells, int (*matrixPtr)[numberOfCells]){
 
         for( int column=0; column< numberOfCells; column++){
 
-            printf("%d", *(*(matrixPtr + row)+column));
+            printf("%d", *(*(matrixPointer + row)+column));
 
             if(column!= numberOfCells-1){
                 printf(",");
@@ -61,19 +61,31 @@ void displayMatrixAs2DArray(int numberOfCells, int (*matrixPtr)[numberOfCells]){
     printf("]\n");
 }
 
+int isValidInput(int numberOfCells){
+
+    if(numberOfCells<=0){
+        return 0;
+    }
+    return 1;
+}
+
 int main(){
     
     int numberOfCells;
 
     printf("Enter Number Of Cells for Square Matrix : ");
-    scanf("%d", &numberOfCells);
+    
+    if(scanf("%d", &numberOfCells) != 1 || !isValidInput(numberOfCells)){
+        printf("Error: Number Of Cells Should Be A Positive Integer.\n");
+        return 1;
+    }
 
     int Matrix[numberOfCells][numberOfCells];
 
-    int (*matrixPtr)[numberOfCells]= Matrix;
+    int (*matrixPointer)[numberOfCells]= Matrix;
 
-    fillSpiralMatrix(numberOfCells, matrixPtr);
-    displayMatrixAs2DArray(numberOfCells, matrixPtr);
+    fillSpiralMatrix(numberOfCells, matrixPointer);
+    displayMatrixAs2DArray(numberOfCells, matrixPointer);
 
     return 0;
 
