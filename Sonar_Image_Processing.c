6311fdad198ddcdadf741e7c3a31b06108fd9f8e
Tanushka Jangid <tanushkajangid583@gmail.com>
2025-10-23 00:29:55 +0530

    Refactor Sonar_Image_Processing.c for clarity
    Included proper spacing and formating.

--- Sonar_Image_Processing.c
+++ Sonar_Image_Processing.c
@@ -2,13 +2,13 @@
 #include<stdlib.h>
 #include<time.h>
 
-const int MIN_CELLS=2;
-const int MAX_CELLS=10;
-const int MAX_INTENSITY_VALUE=255;
+const int MIN_CELLS = 2;
+const int MAX_CELLS = 10;
+const int MAX_INTENSITY_VALUE = 255;
 
 int isValidInput(int numberOfCells){
 
-    if(numberOfCells<MIN_CELLS || numberOfCells>MAX_CELLS){
+    if(numberOfCells < MIN_CELLS || numberOfCells > MAX_CELLS){
         return 0;
     }
 
@@ -20,7 +20,7 @@ void generateRandomImageMatrix(int numberOfCells, int (*imagePointer)[numberOfCe
     srand(time(NULL));
     for (int matrixRow = 0; matrixRow < numberOfCells; matrixRow++) {
         for (int matrixColumn = 0; matrixColumn < numberOfCells; matrixColumn++) {
-            *(*(imagePointer+matrixRow)+matrixColumn) = rand() % MAX_INTENSITY_VALUE;
+            *(*(imagePointer + matrixRow) + matrixColumn) = rand() % MAX_INTENSITY_VALUE;
         }
     }
 }
@@ -48,37 +48,37 @@ void applySmoothingFilter(int numberOfCells, int (*imagePointer)[numberOfCells])
     int previousSmoothenImage[numberOfCells];
     int currentSmoothenImage[numberOfCells];
 
-    for(int rowInRotatedImage=0; rowInRotatedImage<numberOfCells; rowInRotatedImage++){
-        for( int columnInRotatedImage=0; columnInRotatedImage<numberOfCells; columnInRotatedImage++){
-            int sumOfNeighbours=0;
-            int countOfNeighbours=0;
+    for(int rowInRotatedImage = 0; rowInRotatedImage < numberOfCells; rowInRotatedImage++){
+        for(int columnInRotatedImage = 0; columnInRotatedImage < numberOfCells; columnInRotatedImage++){
+            int sumOfNeighbours = 0;
+            int countOfNeighbours = 0;
 
-            for(int neighbourRow= rowInRotatedImage-1; neighbourRow<= rowInRotatedImage+1; neighbourRow++){
+            for(int neighbourRow = rowInRotatedImage - 1; neighbourRow <= rowInRotatedImage + 1; neighbourRow++){
 
-                for(int neighbourColumn= columnInRotatedImage-1; neighbourColumn<= columnInRotatedImage+1; neighbourColumn++){
+                for(int neighbourColumn = columnInRotatedImage - 1; neighbourColumn <= columnInRotatedImage + 1; neighbourColumn++){
 
-                    if(neighbourRow>=0 && neighbourRow<numberOfCells && neighbourColumn>=0 && neighbourColumn<numberOfCells){
+                    if(neighbourRow >= 0 && neighbourRow < numberOfCells && neighbourColumn >= 0 && neighbourColumn<numberOfCells){
 
-                        sumOfNeighbours+= *(*(imagePointer + neighbourRow)+ neighbourColumn);
+                        sumOfNeighbours += *(*(imagePointer + neighbourRow) + neighbourColumn);
                         countOfNeighbours++;
                     }
                 }
             }
-            *(currentSmoothenImage+columnInRotatedImage)= sumOfNeighbours/ countOfNeighbours;
+            *(currentSmoothenImage + columnInRotatedImage) = sumOfNeighbours / countOfNeighbours;
 
         }
-        if(rowInRotatedImage>0){
-            for(int copyColumn=0; copyColumn<numberOfCells; copyColumn++){
-                *(*(imagePointer + (rowInRotatedImage-1))+ copyColumn)= *(previousSmoothenImage+ copyColumn);
+        if(rowInRotatedImage > 0){
+            for(int copyColumn = 0; copyColumn < numberOfCells; copyColumn++){
+                *(*(imagePointer + (rowInRotatedImage - 1)) + copyColumn) = *(previousSmoothenImage + copyColumn);
             }
         }
 
-        for(int copyColumn=0; copyColumn<numberOfCells; copyColumn++){
-            *(previousSmoothenImage+ copyColumn)= *(currentSmoothenImage+ copyColumn);
+        for(int copyColumn = 0; copyColumn < numberOfCells; copyColumn++){
+            *(previousSmoothenImage + copyColumn) = *(currentSmoothenImage + copyColumn);
         }
     }
-    for(int copyColumn=0; copyColumn<numberOfCells; copyColumn++){
-        *(*(imagePointer + (numberOfCells-1))+ copyColumn)= *(previousSmoothenImage+ copyColumn);
+    for(int copyColumn = 0; copyColumn < numberOfCells; copyColumn++){
+        *(*(imagePointer + (numberOfCells - 1)) + copyColumn) = *(previousSmoothenImage + copyColumn);
     }
 }
 
@@ -114,7 +114,7 @@ int main() {
 
     int sonarIntensityValueImage[numberOfCells][numberOfCells];
 
-    int (*imagePointer)[numberOfCells]= sonarIntensityValueImage;
+    int (*imagePointer)[numberOfCells] = sonarIntensityValueImage;
 
     printf("Original Randomly Generated Matrix:\n");
     generateRandomImageMatrix(numberOfCells, imagePointer);
