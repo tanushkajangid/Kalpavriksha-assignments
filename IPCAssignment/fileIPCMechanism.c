3431c68d0816401ccb8aaaa1300a93fff163eaa5
Tanushka Jangid <tanushkajangid583@gmail.com>
2025-12-24 19:08:09 +0530

    Refactor IPC mechanism for sorting array
    Created separate functions for parent and child processes.
    Renamed elements to numberOfElements.

--- IPCAssignment/fileIPCMechanism.c
+++ IPCAssignment/fileIPCMechanism.c
@@ -2,88 +2,85 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include "utilities.h"
 
 #define DATA_FILE "data.txt"
 
-void sortArray(int *array, int size){
-    int temporary;
-    for(int i = 0; i< size-1; i++){
-        for(int j = 0; j < size - i - 1; j++){
-            if(array[j] > array[j+1]){
-                temporary = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temporary;
-            }
-        }
-    }
+void parentProcess(int *numbersArray, int numberOfElements ){
+
+    FILE* filePointer;
+
+    wait(NULL);
+
+    filePointer = fopen(DATA_FILE, "rb");
+
+    fread(&numberOfElements, sizeof(int), 1, filePointer);
+    fread(numbersArray, sizeof(int), numberOfElements, filePointer);
+    fclose(filePointer);
+
+    printf("After Sorting:\n");
+    displayArray(numbersArray, numberOfElements);
 }
 
-void displayArray(int *array, int size){
+void childProcess(){
 
-    for(int i = 0; i < size; i++){
-        printf("%d ", array[i]);
-    }
-    printf("\n");
+    int numberOfElements;
+    FILE *filePointer;
+
+    filePointer = fopen(DATA_FILE, "rb");
+    fread(&numberOfElements, sizeof(int), 1, filePointer);
+
+    int *childArray = malloc(numberOfElements * sizeof(int));
+
+    fread(childArray, sizeof(int), numberOfElements, filePointer);
+    fclose(filePointer);
+
+    sortArray(childArray, numberOfElements);
+
+    filePointer = fopen(DATA_FILE, "wb");
+
+    fwrite(&numberOfElements, sizeof(int), 1, filePointer);
+    fwrite(childArray, sizeof(int), numberOfElements, filePointer);
+    fclose(filePointer);
+
+    free(childArray);
+    exit(0);
 
 }
 
 int main(){
 
-    int elements;
+    int numberOfElements;
     printf("Enter number of elements: ");
-    scanf("%d", &elements);
+    scanf("%d", &numberOfElements);
 
-    int *numbersArray = malloc(elements * sizeof(int));
+    int *numbersArray = malloc(numberOfElements * sizeof(int));
 
     printf("Enter Elements:\n");
-    for(int i = 0; i < elements; i++){
+    for(int i = 0; i < numberOfElements; i++){
         scanf("%d", &numbersArray[i]);
     }
 
     printf("Before Sorting:\n");
-    displayArray(numbersArray, elements);
+    displayArray(numbersArray, numberOfElements);
 
     FILE *filePointer;
     filePointer = fopen(DATA_FILE, "wb");
 
-    fwrite(&elements, sizeof(int), 1, filePointer);
-    fwrite(numbersArray, sizeof(int), elements, filePointer);
+    fwrite(&numberOfElements, sizeof(int), 1, filePointer);
+    fwrite(numbersArray, sizeof(int), numberOfElements, filePointer);
     fclose(filePointer);
 
-    if(fork() == 0){ //child Process
+    if(fork() == 0){
 
-        filePointer = fopen(DATA_FILE, "rb");
-        fread(&elements, sizeof(int), 1, filePointer);
+        childProcess();
 
-        int *childArray = malloc(elements * sizeof(int));
-
-        fread(childArray, sizeof(int), elements, filePointer);
-        fclose(filePointer);
-
-        sortArray(childArray, elements);
-
-        filePointer = fopen(DATA_FILE, "wb");
-
-        fwrite(&elements, sizeof(int), 1, filePointer);
-        fwrite(childArray, sizeof(int), elements, filePointer);
-        fclose(filePointer);
-
-        free(childArray);
-        exit(0);
     }
 
-    else{  //parent Process
+    else{
          
-        wait(NULL);
-
-        filePointer = fopen(DATA_FILE, "rb");
-
-        fread(&elements, sizeof(int), 1, filePointer);
-        fread(numbersArray, sizeof(int), elements, filePointer);
-        fclose(filePointer);
-
-        printf("After Sorting:\n");
-        displayArray(numbersArray, elements);
+        parentProcess(numbersArray, numberOfElements);
+        
     }
 
     free(numbersArray);
